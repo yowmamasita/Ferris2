dc981feeb926f5cc0d268137704601edc105187c
Jon Wayne Parrott <jjramone13@gmail.com>
2013-04-06 14:27:22 -0400

    Auto model loading

--- ferris/core/scaffold.py
+++ ferris/core/scaffold.py
@@ -23,6 +23,9 @@ class Scaffolding(object):
 
         setattr(self.handler, 'scaffold', self.handler.Scaffold(self.handler))
 
+        if not hasattr(self.handler.Meta, 'Model'):
+            _load_model(self.handler)
+
 
 class Scaffold(object):
     """
@@ -31,6 +34,24 @@ class Scaffold(object):
     def __init__(self, handler):
         if not hasattr(self, 'plural'):
             self.plural = inflector.pluralize(handler.name)
+
+
+# Utility Functions
+
+
+def _load_model(handler):
+    import_form_base = '.'.join(handler.__module__.split('.')[:-2])
+    # Attempt to import the model automatically
+    model_name = inflector.singularize(handler.__class__.__name__)
+    try:
+        module = __import__('%s.models.%s' % (import_form_base, inflector.underscore(model_name)), fromlist=['*'])
+        setattr(handler.Meta, 'Model', getattr(module, model_name))
+        setattr(handler.meta, 'Model', getattr(module, model_name))
+    except (ImportError, AttributeError):
+        raise RuntimeError("Scaffold coudn't automatically determine a model class for handler %s, please assign it a Meta.Model class variable." % handler.__class__.__name__)
+
+
+# Handler Methods
 
 
 def list(handler):
