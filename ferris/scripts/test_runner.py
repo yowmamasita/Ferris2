47bcaf41a676f20331668aabf0cfb7f54aca78be
Jon Wayne Parrott <jjramone13@gmail.com>
2013-04-22 09:38:44 -0400

    Fixing test runner

--- ferris/scripts/test_runner.py
+++ ferris/scripts/test_runner.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python2.7
 import sys
-import pkg_resources
 import argparse
 import os
+import re
+import logging
 
 # Fix the path
 base_dir = os.path.abspath(os.path.join(
@@ -12,10 +13,36 @@ base_dir = os.path.abspath(os.path.join(
 sys.path.insert(0, base_dir)
 
 
+def bootstrap(sdk_path):
+    sys.path.insert(0, sdk_path)
+
+    # Force the google module to reload, for some reason on windows this can sometimes point to the one in site-packages.
+    import google
+    reload(google)
+
+    import dev_appserver
+    dev_appserver.fix_sys_path()
+    sys.path = [re.sub('webob_0_9', 'webob_1_1_1', x) for x in sys.path]
+
+    import fix_imports
+    (fix_imports)
+
+    # Activate a testbed so that httplib2 always knows that it's in app engine
+    from google.appengine.ext import testbed
+    testbed = testbed.Testbed()
+    testbed.activate()
+    testbed.init_urlfetch_stub()
+
+    # Remove agressive logging
+    rootLogger = logging.getLogger()
+    for handler in rootLogger.handlers:
+        if isinstance(handler, logging.StreamHandler):
+            rootLogger.removeHandler(handler)
+
+
 def main(sdk_path, package, coverage=False):
     test_path = os.path.join(base_dir, package, 'tests')
 
-    from ferris.tests.bootstrapper import bootstrap
     bootstrap(sdk_path)
 
     import nose
