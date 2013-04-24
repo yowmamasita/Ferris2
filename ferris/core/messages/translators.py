afdc936b0b4df6a978569a70a8f75e1902bcb933
Jon Wayne Parrott <jjramone13@gmail.com>
2013-04-24 11:42:21 -0400

    Translators can now work with classes or instances

--- ferris/core/messages/translators.py
+++ ferris/core/messages/translators.py
@@ -1,3 +1,4 @@
+import inspect
 from protorpc import messages
 from .converters import converters as default_converters
 
@@ -9,8 +10,8 @@ def _common_fields(entity, message):
     return message_fields, entity_properties, fields
 
 
-def entity_to_message(entity, message_type, converters=None):
-    message_fields, entity_properties, fields = _common_fields(entity, message_type)
+def entity_to_message(entity, message, converters=None):
+    message_fields, entity_properties, fields = _common_fields(entity, message)
 
     converters = dict(default_converters.items() + converters.items()) if converters else default_converters
 
@@ -25,17 +26,22 @@ def entity_to_message(entity, message_type, converters=None):
             value = converter.to_message(entity, property, value)
             values[field] = value
 
-    return message_type(**values)
+    if inspect.isclass(message):
+        return message(**values)
+    else:
+        for name, value in values.iteritems():
+            setattr(message, name, value)
+        return message
 
 
-def message_to_entity(message, model_type, converters=None):
-    message_fields, entity_properties, fields = _common_fields(model_type, message)
+def message_to_entity(message, model, converters=None):
+    message_fields, entity_properties, fields = _common_fields(model, message)
 
     converters = dict(default_converters.items() + converters.items()) if converters else default_converters
 
     values = {}
     for field in fields:
-        property = model_type._properties[field]
+        property = model._properties[field]
         value = getattr(message, field)
 
         converter = converters[property.__class__]
@@ -44,7 +50,11 @@ def message_to_entity(message, model_type, converters=None):
             value = converter.to_model(message, field, value)
             values[field] = value
 
-    return model_type(**values)
+    if inspect.isclass(model):
+        return model(**values)
+    else:
+        model.populate(**values)
+        return model
 
 
 def model_message(Model, only=None, exclude=None, converters=None):
