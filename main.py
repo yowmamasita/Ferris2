2848cf693c95460b4ff1afffd635dab5162ffef9
Jon Wayne Parrott <jjramone13@gmail.com>
2013-08-19 23:13:07 -0400

    Creating our own deferred handler that takes care of import issues

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import fix_imports
 import settings
 import ferris
 import ferris.app
+import ferris.deferred_app
 import ferris.routes
 import app.routes
 import app.listeners
 
-app = ferris.app.app  # the app object in the app package.
+main_app = ferris.app.app  # Main application
+deferred_app = ferris.deferred_app.app  # Deferred application
 
 # from google.appengine.ext.appstats import recording
 # app = recording.appstats_wsgi_middleware(app)
