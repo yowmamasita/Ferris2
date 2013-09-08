bb448b3721661c11d244b3899b3535bfbf6a41c4
Jon Wayne Parrott <jjramone13@gmail.com>
2013-09-07 21:01:36 -0400

    Adding test for search and pagination

--- ferris/tests/test_pagination.py
+++ ferris/tests/test_pagination.py
@@ -6,51 +6,60 @@ from ferris.core.ndb import Model, ndb
 from ferris.components import pagination, search
 from ferris.behaviors import searchable
 from ferris.core import messages
+from google.appengine.api.search import SortOptions, SortExpression
 
 
-class Widget(Model):
+class Sprocket(Model):
     class Meta:
         behaviors = (searchable.Searchable,)
 
+    stamp = ndb.IntegerProperty()
     name = ndb.StringProperty()
 
 
-class Widgets(Controller):
+class Sprockets(Controller):
     class Meta:
         prefixes = ('api',)
         components = (scaffold.Scaffolding, pagination.Pagination, messages.Messaging, search.Search)
         pagination_limit = 10
-        Model = Widget
+        Model = Sprocket
 
     list = scaffold.list
     api_list = scaffold.list
 
     @route
     def api_search(self):
-        self.components.search()
+        sort_options = SortOptions(expressions=[
+            SortExpression(
+                expression='stamp',
+                direction=SortExpression.ASCENDING
+            )
+        ])
+        self.components.search(options={'sort_options': sort_options})
 
 
 class TestPagination(FerrisTestCase):
     def setUp(self):
         super(TestPagination, self).setUp()
-        Widgets._build_routes(self.testapp.app.router)
+        Sprockets._build_routes(self.testapp.app.router)
 
-        # Create a few pages of widgets
+        # Create a few pages of sprokets
         for i in range(1, 50):
-            Widget(name=str(i)).put()
+            Sprocket(name=str(i), stamp=i).put()
 
     def testNormalList(self):
-        r = self.testapp.get('/widgets')
+        r = self.testapp.get('/sprockets')
         assert '11' not in r
 
-        _, cursor, _ = Widget.query().fetch_page(10)
+        _, cursor, _ = Sprocket.query().fetch_page(10)
 
         assert cursor
-        r = self.testapp.get('/widgets?cursor=%s' % cursor.urlsafe())
+        r = self.testapp.get('/sprockets?cursor=%s' % cursor.urlsafe())
         assert '11' in r
 
     def testApiList(self):
-        r = self.testapp.get('/api/widgets')
+
+        r = self.testapp.get('/api/sprockets')
         assert '10' in r
         assert '11' not in r
 
@@ -66,15 +75,15 @@ class TestPagination(FerrisTestCase):
         assert 'previous_page' in r.json
         assert r.json['page'] == 2
 
-    # def testSearchList(self):
-    #     r = self.testapp.get('/api/widgets/search')
-    #     assert '10' in r
+    def testSearchList(self):
+        r = self.testapp.get('/api/sprockets/search')
+        assert '10' in r
 
-    #     assert len(r.json['items']) == 10
-    #     assert r.json['count'] == 10
-    #     assert r.json['limit'] == 10
-    #     assert r.json['next_page']
+        assert len(r.json['items']) == 10
+        assert r.json['count'] == 10
+        assert r.json['limit'] == 10
+        assert r.json['next_page']
 
-    #     r = self.testapp.get(r.json['next_page'])
-    #     print r
-    #     assert '11' in r
+        r = self.testapp.get(r.json['next_page'])
+        print [x['name'] for x in r.json['items']]
+        assert '11' in r
