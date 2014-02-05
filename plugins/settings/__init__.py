21c0578285f2beaae4eefdd36d5ee89d6379394c
Jon Wayne Parrott <jjramone13@gmail.com>
2014-02-05 03:52:56 -0500

    Adding localcache to reduce the load on memcache

--- plugins/settings/__init__.py
+++ plugins/settings/__init__.py
@@ -1,11 +1,15 @@
 from ferris.core import plugins
+import threading
 
 plugins.register('settings')
 
 
 from .models.setting import Setting as SettingModel
 
+local_cache = threading.local()
+
 
 def activate(settings):
-    overrides = SettingModel.get_settings()
-    settings.update(overrides)
+    if not hasattr(local_cache, 'overrides'):
+        local_cache.overrides = SettingModel.get_settings()
+    settings.update(local_cache.overrides)
