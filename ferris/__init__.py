9df31d1c306ec288e8c5fcffdcdfa63df1bc71ec
Jon Wayne Parrott <jjramone13@gmail.com>
2014-02-05 02:38:56 -0500

    Updating version

--- ferris/__init__.py
+++ ferris/__init__.py
@@ -1,4 +1,4 @@
-version = "2.0.0"
+version = "2.0.1"
 
 from . import core, components, behaviors
 from core import scaffold, events, routing, oauth2, forms, messages, inflector, settings, plugins, views
