ea03abe2c87e5bbfdca7ac297096285bbb6c0b9b
Jon Wayne Parrott <jjramone13@gmail.com>
2014-05-08 00:09:19 -0400

    Adding API client build helper

--- ferris/core/google_api_helper.py
+++ ferris/core/google_api_helper.py
@@ -2,16 +2,71 @@ import httplib2
 import logging
 import json
 import functools
-from apiclient import errors
+from apiclient import discovery, errors
+import hashlib
 
 
-def google_api_retries(f):
+def build(serviceName, version, credentials):
+    """
+    Build a Google API client and caches it in the in-process cache. This reduces
+    the number of calls to the discovery API as well as making it easy to share
+    the client across multiple parts of code with little effort.
+
+    Usage is exactly like apiclient.discovery.build::
+
+        drive = build("drive", "v2", credentials)
+
+    """
+    from ferris import caching
+    credentials_hash = hashlib.sha1(credentials.to_json()).hexdigest()
+    cache_key = "ferris:google-client-%s-%s-%s" % (serviceName, version, credentials_hash)
+
+    @caching.cache_using_local(cache_key)
+    def inner():
+        http = httplib2.Http()
+        credentials.authorize(http)
+        service = discovery.build(serviceName, version, http=http)
+        return service
+
+    return inner()
+
+
+def retry_execute(request):
+    """
+    Executes the given request from the Google API client and applies the
+    appropriate retry policy. This ensures that if your request fails due to
+    internal server error or quota denial the request will be automatically
+    retried.
+
+    Example::
+
+        request = service.files().list()
+        result = retry_execute(request)
+
+    """
+    @retries
+    def inner():
+        return request.execute()
+    return inner()
+
+
+def retries(f):
     """
     Shortcut decorator that uses the appropraite retry policy for dealing with Google APIs.
 
     Will retry if an HttpError in the 5xx range is raise, but will fail if the error is in the 4xx range.
+
+    This is useful over retry_execute because it can retry an entire function, not just a single request.
+
+    Example::
+
+        @retries
+        def rename_file():
+            client = build('drive', 'v2')
+            client.files.update(fileId="123", data={"name": "Test"}).execute()
+
     """
-    from ferris import retries
+    from ferris import retries as ferris_retries
 
     @functools.wraps(f)
     def inner(*args, **kwargs):
@@ -23,7 +78,7 @@ def google_api_retries(f):
             logging.error("Non-recoverable exception: %s" % error)
             raise
 
-    r_inner = retries(max_tries=5, should_retry=apiclient_retry_policy, delay=1, backoff=2)(inner)
+    r_inner = ferris_retries(max_tries=5, should_retry=apiclient_retry_policy, delay=1, backoff=2)(inner)
     return r_inner
 
 
@@ -65,7 +120,6 @@ def get_discovery_document(api, api_version, uri_template="https://www.googleapi
 
 
 def patch_discovery():
-    from apiclient import discovery
     original_build = discovery.build
 
     def patched_build(serviceName, version, http=None, **kwargs):
