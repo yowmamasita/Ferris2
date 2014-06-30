a4a70633ee0cb52d1f99a0e6ff36a3bf77c9bfac
Jon Wayne Parrott <jjramone13@gmail.com>
2014-06-30 15:56:50 -0400

    Adding text body to core.mail, resolving #52. (Enjoy, Ken!)

--- ferris/tests/test_email.py
+++ ferris/tests/test_email.py
@@ -5,7 +5,7 @@ from ferris.core import mail
 class MailTest(AppEngineTest):
 
     def test(self):
-        res, body = mail.send_template(
+        res, body, text_body = mail.send_template(
             sender='user@example.com',
             recipient='test@example.com',
             subject='Hello!',
@@ -15,3 +15,6 @@ class MailTest(AppEngineTest):
             })
 
         assert 'Doctor' in body
+        assert 'Doctor' in text_body
+        assert '<h1>' in body
+        assert '<h1>' not in text_body
